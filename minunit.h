c63c75908f54058f4f30f879674f7a87d5454d86
Mehow <Kaiser.mehow@gmail.com>
2015-03-25 15:08:18 +0000

    Wrapping test variable in brackets to make marco safer Prevents a test like 1==0 being evaluated as (!1)==0

--- minunit.h
+++ minunit.h
@@ -6,7 +6,7 @@ int run_cases(test_case * test_cases, int num_of_test_cases);
 
 #define mu_assert(message, test, ...)\
 do {\
-    if (!test){\
+    if (!(test)){\
         printf(message, ##__VA_ARGS__);\
         return 1;\
     }\
