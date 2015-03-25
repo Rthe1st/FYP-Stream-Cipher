2997977365f4959943c0ca53c86cd3501de7f23a
Mehow <Kaiser.mehow@gmail.com>
2015-03-25 15:14:59 +0000

    Completing unit tests fro mobius attack Fixing bugs found for it

--- mobius_cube_attack.c
+++ mobius_cube_attack.c
@@ -10,34 +10,38 @@ const int MIN_NUM_AXES = 0;
 Max_terms_list *mobius_find_max_terms(int max_term_limit, size_t dimension_limit, const Cipher_info * const cipher_info) {
     int *dimensions = calloc(dimension_limit, sizeof(int));
     Max_terms_list *max_terms_list = malloc(sizeof(Max_terms_list));
-    max_terms_list->max_terms = malloc(0);
+    max_terms_list->max_terms = malloc(sizeof(Max_term)*0);
     max_terms_list->max_term_count = 0;
     uint64_t zeroed_key[2] = {0, 0};
-    while (max_terms_list->max_term_count < max_term_limit && dimensions[dimension_limit - 1] < cipher_info->iv_size + dimension_limit) {
+    for (int i = 0; i < dimension_limit; i++) {
+        dimensions[i] = i;
+    }
+    while (max_terms_list->max_term_count < max_term_limit && dimensions[dimension_limit - 1] < cipher_info->iv_size) {
         int *zeroed_key_poly = mobius_transform(dimensions, dimension_limit, MIN_NUM_AXES, zeroed_key, cipher_info);
         int *linear_results = mobius_is_super_poly_linear(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
-        Max_terms_list* cube_max_terms = mobius_construct_max_terms(zeroed_key_poly, dimensions, max_terms_list->max_term_count, cipher_info);
+        Max_terms_list* cube_max_terms = mobius_construct_max_terms(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
         for (int i = 0; i < cube_max_terms->max_term_count; i++) {
             printf("checking cube terms, overall max term count %d\n", max_terms_list->max_term_count);
             Max_term current_max_term = cube_max_terms->max_terms[i];
+            int e=0;//commenting this out cause errors, whyyyyy
+            //varibale name can be anything
             if (current_max_term.numberOfTerms > 0 && linear_results[i] == 1) {
-                printf("cube termbpassed check\n");
                 max_terms_list->max_term_count++;
                 //todo: use size doubling buffer to save reallocing too much
-                max_terms_list->max_terms = realloc(max_terms_list->max_terms, max_terms_list->max_term_count);
+                max_terms_list->max_terms = realloc(max_terms_list->max_terms, sizeof(Max_term) * max_terms_list->max_term_count);
                 max_terms_list->max_terms[max_terms_list->max_term_count-1] = current_max_term;
+            }else{
+                free_max_term(&current_max_term);
             }
         }
         free(zeroed_key_poly);
         free(linear_results);
-        for(int i = 0; i < cube_max_terms->max_term_count; i++){
-            free_max_term(&(cube_max_terms->max_terms[i]));
-        }
         free(cube_max_terms);
         //this wont search the dimensio space exhaustivly
         //because say cube {1,2,3} and {4,5,6} will be search but not {1,2,4}
         //to correct this, come up with a system for heuistly finding good cubes
         //or searching exhustivly and remebering past sub cubes for reuse
+
         for (int i = 0; i < dimension_limit; i++) {
             dimensions[i] += dimension_limit;
         }
@@ -67,22 +71,24 @@ int *mobius_is_super_poly_linear(int *zeroed_key_super_poly, int *cube_axes, int
     //but this require storing power(2, axis)*(n+2) values
     //to save space, compute key a, compare to keys 1-n
     //require power(2, axis)*4 values to be stored but superpolys are recomputed multiple times
-    for (int key_index_1 = 0; key_index_1 < number_of_keys - 1; key_index_1++) {
+    for (int key_index_1 = 0; key_index_1 < number_of_keys; key_index_1++) {
         int *key_1_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, keys[key_index_1], cipher_info);
         for (int key_index_2 = key_index_1 + 1; key_index_2 < number_of_keys; key_index_2++) {
             int *key_2_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, keys[key_index_2], cipher_info);
-            uint64_t combined_keys[2] = {keys[key_index_1][0] + keys[key_index_2][0], keys[key_index_1][1] + keys[key_index_2][1]};
+            uint64_t combined_keys[2] = {keys[key_index_1][0] ^ keys[key_index_2][0], keys[key_index_1][1] ^ keys[key_index_2][1]};
             int *pre_combined_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, combined_keys, cipher_info);
             int all_results_non_linear = 0;
             for (int i = 0; i < power(2, cube_dimension); i++) {
                 if ((pre_combined_superpolys[i] ^ zeroed_key_super_poly[i]) != (key_1_superpolys[i] ^ key_2_superpolys[i])) {
-                    results[i] = 0;
+                    if (results[i] == 1) {
+                        all_results_non_linear += results[i];
+                        results[i] = 0;
+                    }
                 }
-                all_results_non_linear += results[i];
             }
             free(pre_combined_superpolys);
             free(key_2_superpolys);
-            if (all_results_non_linear != 0) {
+            if (all_results_non_linear == power(2, cube_dimension)) {
                 free(key_1_superpolys);
                 return results;
             }
@@ -97,7 +103,6 @@ int *mobius_is_super_poly_linear(int *zeroed_key_super_poly, int *cube_axes, int
 }
 
 Max_terms_list* mobius_construct_max_terms(int *zeroed_super_polys, int *dimensions, int dimension_count, const Cipher_info * const cipher_info) {
-    //iv gives a linear super poly, so now find what its terms are
     uint64_t term_key[2] = {0, 0};
     Max_terms_list *max_terms_list = malloc(sizeof(Max_terms_list));
     max_terms_list->max_term_count = power(2, dimension_count);
@@ -115,12 +120,9 @@ Max_terms_list* mobius_construct_max_terms(int *zeroed_super_polys, int *dimensi
         term_key[0] = 0;
         term_key[1] = 0;
         set_bit(term_key, 1, key_bit);
-        printf("key bit %d\n", key_bit);
         int *key_super_poly = mobius_transform(dimensions, dimension_count, MIN_NUM_AXES, term_key, cipher_info);
         for (int g = 0; g < power(2, dimension_count); g++) {
             Max_term *current_max_term = &(max_terms_list->max_terms[g]);
-            printf("mobius super res: %d ", key_super_poly[g]);
-            printf("mobius zeroded: %d\n", zeroed_super_polys[g]);
             if (key_super_poly[g] + zeroed_super_polys[g] == 1) {
                 current_max_term->terms[current_max_term->numberOfTerms] = key_bit;
                 current_max_term->numberOfTerms++;
@@ -187,7 +189,7 @@ int *mobius_transform(const int const *cube_axis, const int num_of_axis, const i
     for (int current_dimension = 0; current_dimension < num_of_axis; current_dimension++) {
         int size = (int) power(2, current_dimension);
         for (int pos = 0; pos < power(2, num_of_axis); pos += 2 * size) {
-            for (int j = 0; j < size - 1; j++) {
+            for (int j = 0; j < size; j++) {
                 debug_print("table update: \n");
                 for(int i =0;i< derivative_count;i++) {
                     debug_print("row %d: %d\n",i, derivative_results[i]);
--- mobius_unit_tests.c
+++ mobius_unit_tests.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <time.h>
 
 #include "minunit.h"
 #include "mobius_cube_attack.h"
@@ -9,6 +10,52 @@
 #include "dummy_cipher.h"
 
 int tests_run = 0;
+
+static int test_mobius_is_super_poly_linear(){
+    srand((unsigned int) time(NULL));
+    printf("testing mobius_is_super_poly_linear\n");
+    Cipher_info * cipher_info = dummy_info();
+    int dimension_limit = 1;
+    int dimensions[10] = {0};
+    uint64_t zeroed_key[1] = {0};
+    cipher_info->init_clocks = 0;
+    int *zeroed_key_poly = mobius_transform(dimensions, dimension_limit, 0, zeroed_key, cipher_info);
+    int* linear_results = mobius_is_super_poly_linear(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
+    int* expected_results = malloc(sizeof(int)*2);
+    expected_results[0] = 0;
+    expected_results[1] = 1;
+    for(int i=0; i<power(2, dimension_limit); i++){
+        mu_assert("clock 1, linear result[%d] == %d, expected %d\n", linear_results[i] == expected_results[i], i, linear_results[i], expected_results[i]);
+    }
+    free(linear_results);
+    free(zeroed_key_poly);
+    free(expected_results);
+    dimension_limit = 4;
+    dimensions[0] = 0; dimensions[1] = 1; dimensions[2] = 2; dimensions[3] = 3;
+    cipher_info->init_clocks = 5;
+    zeroed_key_poly = mobius_transform(dimensions, dimension_limit, 0, zeroed_key, cipher_info);
+    linear_results = mobius_is_super_poly_linear(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
+    expected_results = malloc(sizeof(int)*16);
+    expected_results[0] = 0;
+    expected_results[1] = 0;
+    expected_results[2] = 0;
+    expected_results[3] = 0;
+    expected_results[4] = 0;
+    expected_results[5] = 1;
+    expected_results[6] = 1;
+    expected_results[7] = 1;
+    expected_results[8] = 1;
+    for(int i=9;i<16;i++){
+        expected_results[i] = 1;
+    }
+    for(int i=0; i<power(2, dimension_limit); i++){
+        mu_assert("clock 6, linear result[%d] == %d, expected %d\n", linear_results[i] == expected_results[i], i, linear_results[i], expected_results[i]);
+    }
+    free(linear_results);
+    free(zeroed_key_poly);
+    printf("done testing mobius_is_super_poly_linear\n");
+    return 0;
+};
 
 static int test_mobius_construct_max_terms(){
     printf("testing construct max terms\n");
@@ -21,13 +68,14 @@ static int test_mobius_construct_max_terms(){
     Max_terms_list* cube_max_terms = mobius_construct_max_terms(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
     mu_assert("1 clock, maxterm count of %d != 2\n", cube_max_terms->max_term_count == 2, cube_max_terms->max_term_count);
     Max_term chosen_max_term = cube_max_terms->max_terms[0];
-    printf("max term[0] term[0] == %d plus one %d\n",chosen_max_term.terms[0], chosen_max_term.plusOne);
-    mu_assert("1 clock, maxterm[0], num terms == %d instead of 0\n", chosen_max_term.numberOfTerms == 3, chosen_max_term.numberOfTerms);
+    mu_assert("1 clock, maxterm[0], num terms == %d instead of 1\n", chosen_max_term.numberOfTerms == 1, chosen_max_term.numberOfTerms);
+    mu_assert("1 clock, maxterm[0], terms[0] == %d, not 3\n", chosen_max_term.terms[0] == 3, chosen_max_term.terms[0]);
     mu_assert("1 clock, maxterm[0], plusone != 0\n", chosen_max_term.plusOne == 0);
     chosen_max_term = cube_max_terms->max_terms[1];
     mu_assert("1 clock, maxterm[1], num terms == %d instead of 1\n", chosen_max_term.numberOfTerms == 1, chosen_max_term.numberOfTerms);
     mu_assert("1 clock, maxterm[1], terms[0] != 0\n", chosen_max_term.terms[0] == 0);
     mu_assert("1 clock, maxterm[1], plusone != 0\n", chosen_max_term.plusOne == 0);
+    free(zeroed_key_poly);
     for(int i=0;i<cube_max_terms->max_term_count;i++){
         free_max_term(&(cube_max_terms->max_terms[i]));
     }
@@ -35,6 +83,7 @@ static int test_mobius_construct_max_terms(){
     dimension_limit = 3;
     dimensions[0] = 0; dimensions[1] = 1; dimensions[2] = 2;
     cipher_info->init_clocks = 5;
+    zeroed_key_poly = mobius_transform(dimensions, dimension_limit, 0, zeroed_key, cipher_info);
     cube_max_terms = mobius_construct_max_terms(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
     mu_assert("6 clocks, max term count != 3\n", cube_max_terms->max_term_count == 8);
     chosen_max_term = cube_max_terms->max_terms[7];
@@ -44,7 +93,7 @@ static int test_mobius_construct_max_terms(){
     mu_assert("failed for 6 init clocks results[6](110) num of terms != 1\n", chosen_max_term.numberOfTerms == 1);
     mu_assert("failed for 6 init clocks results[6](110) term[0] != 3\n",chosen_max_term.terms[0] == 3);
     chosen_max_term = cube_max_terms->max_terms[3];
-    mu_assert("failed for 6 init clocks results[3](11) num terms != 0\n", chosen_max_term.numberOfTerms == 0);
+    mu_assert("failed for 6 init clocks results[3](011) num terms != 0\n", chosen_max_term.numberOfTerms == 0);
     for(int i=0;i<cube_max_terms->max_term_count;i++){
         free_max_term(&(cube_max_terms->max_terms[i]));
     }
@@ -64,6 +113,12 @@ static int test_mobius_transform(){
     int* results = mobius_transform(axis, num_of_axis, num_of_axis, key, cipher_info);
     mu_assert("superpoly results[0] != 0 && results[1] != 0 for key {0} clocks 1, axis {0}\n", results[0] == 0 && results[1] == 0);
     free(results);
+    key[0] = power(2, 0) + power(2,3);
+    num_of_axis = 1;
+    axis[0] = 0;
+    results = mobius_transform(axis, num_of_axis, num_of_axis, key, cipher_info);
+    mu_assert("superpoly results[0] == %d, expected %d && results[1] == %d, expected %d for key {power(2, 0) + power(2,3)} clocks 1, axis {0}\n", results[0] == 1 && results[1] == 1,results[0], 1, results[1], 1);
+    free(results);
     cipher_info->init_clocks = 5;
     key[0] = power(2, 5)-1;
     num_of_axis = 1;
@@ -76,14 +131,71 @@ static int test_mobius_transform(){
     axis[0] = 0; axis[1] = 1; axis[2] = 2;
     key[0] = power(2, 1) + power(2, 3);
     results = mobius_transform(axis, num_of_axis, num_of_axis, key, cipher_info);
-    mu_assert("failed for 6 init clocks results[7](111)== 1\n", results[7]== 1);
+    mu_assert("failed for 6 init clocks results[0](0)== 1\n", results[0]== 0);
     mu_assert("failed for 6 init clocks results[6](110)== 1\n", results[6]== 1);
-    mu_assert("failed for 6 init clocks results[3](11)== 0\n", results[3]== 0);
+    mu_assert("failed for 6 init clocks results[7](111)== 1\n", results[7]== 0);
+    key[0] = power(2, 0) + power(2, 1);
+    results = mobius_transform(axis, num_of_axis, num_of_axis, key, cipher_info);
+    mu_assert("failed for 6 init clocks results[1](11) == %d, expected 1\n", results[1] == 0, results[1]);
     printf("done testing mobius transform\n");
     return 0;
 }
 
+int test_mobius_find_max_terms(){
+    printf("testing mobois find max terms\n");
+    Cipher_info * cipher_info = dummy_info();
+    cipher_info->init_clocks = 0;
+    int dimension_limit = 1;
+    int max_term_limit = 1;
+    Max_terms_list* max_terms_list = mobius_find_max_terms(max_term_limit,dimension_limit, cipher_info);
+    Max_term * max_terms = max_terms_list->max_terms;
+    int expected_max_term_count;
+    int expected_term_count;
+    int expected_term_values[5];
+    int expected_iv;
+    expected_max_term_count = 1;
+    expected_term_count = 1;
+    expected_term_values[0] = 0;
+    expected_iv = 1;//1 not 0 because the bit index indicates the iv, so 00001 is iv[0], 00010 is iv[1], 00000 is no ivs used
+    mu_assert("failed for 0 clocks, max_term_count was %d not %d\n", max_terms_list->max_term_count == expected_max_term_count, max_terms_list->max_term_count, expected_max_term_count);
+    mu_assert("failed for 0 clocks, max_term[0] term count was %d not %d\n", max_terms[0].numberOfTerms == expected_term_count, max_terms[0].numberOfTerms, expected_term_count);
+    mu_assert("failed for 0 clocks, max_term[0] term[0] was %d not %d\n", max_terms[0].terms[0] == expected_term_values[0], max_terms[0].terms[0], expected_term_values[0]);
+    mu_assert("failed for 0 clocks, max_term[0] iv was %"PRIu64" not %d\n", max_terms[0].iv[0] == expected_iv, max_terms[0].iv[0], expected_iv);
+    cipher_info->init_clocks = 5;
+    max_terms_list = mobius_find_max_terms(5, 4, cipher_info);
+    max_terms = max_terms_list->max_terms;
+    Max_term chosen_max_term = max_terms[0];
+    expected_max_term_count = 3;
+    printf("num of max terms %d\n", max_terms_list->max_term_count);
+    mu_assert("failed for 6 clocks, max_term_count was %d not %d\n", max_terms_list->max_term_count == expected_max_term_count, max_terms_list->max_term_count, expected_max_term_count);
+    expected_term_count = 1;
+    printf("chiosen.number of terms %d\n", chosen_max_term.numberOfTerms);
+    mu_assert("failed for 6 clocks, max_term[0] term count was %d not %d\n", chosen_max_term.numberOfTerms == expected_term_count, chosen_max_term.numberOfTerms, expected_term_count);
+    expected_term_values[0] = 3;
+    mu_assert("failed for 6 clocks, max_term[0] term[0] was %d not %d\n", chosen_max_term.terms[0] ==  expected_term_values[0], chosen_max_term.terms[0], expected_term_values[0]);
+    expected_iv = 6;
+    mu_assert("failed for 6 clocks, max_term[0] iv was %"PRIu64" not %d\n", chosen_max_term.iv[0] == expected_iv, chosen_max_term.iv[0], expected_iv);
+    //
+    chosen_max_term = max_terms[1];
+    expected_term_count = 1;
+    mu_assert("failed for 6 clocks, max_term[1] term count was %d not %d\n", chosen_max_term.numberOfTerms == expected_term_count, chosen_max_term.numberOfTerms, expected_term_count);
+    expected_term_values[0] = 0;
+    mu_assert("failed for 6 clocks, max_term[1] term[0] was %d not %d\n", chosen_max_term.terms[0] == expected_term_values[0], chosen_max_term.terms[0], expected_term_values[0]);
+    expected_iv = 7;
+    mu_assert("failed for 6 clocks, max_term[1] iv was %"PRIu64" not %d\n", chosen_max_term.iv[0] == expected_iv, chosen_max_term.iv[0], expected_iv);
+    //
+    chosen_max_term = max_terms[2];
+    expected_term_count = 1;
+    mu_assert("failed for 6 clocks, max_term[2] term count was %d not %d\n", chosen_max_term.numberOfTerms == expected_term_count, chosen_max_term.numberOfTerms, expected_term_count);
+    expected_term_values[0] = 3;
+    mu_assert("failed for 6 clocks, max_term[2] term[0] was %d not %d\n", chosen_max_term.terms[0] ==  expected_term_values[0], chosen_max_term.terms[0], expected_term_values[0]);
+    expected_iv = 8;
+    mu_assert("failed for 6 clocks, max_term[2] iv was %"PRIu64" not %d\n", chosen_max_term.iv[0] == expected_iv, chosen_max_term.iv[0], expected_iv);
+    printf("done mobois find max terms\n");
+    return 0;
+}
+
 int main(int argc, char **argv) {
-    test_case test_cases[2] = {test_mobius_transform, test_mobius_construct_max_terms};
+    test_case test_cases[4] = {test_mobius_transform, test_mobius_construct_max_terms, test_mobius_is_super_poly_linear, test_mobius_find_max_terms};
     run_cases(test_cases, (sizeof test_cases/ sizeof(test_case)));
 }
