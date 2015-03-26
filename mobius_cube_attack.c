da6d4acbecaeb264edc0beaf3d236bc230576e82
Mehow <Kaiser.mehow@gmail.com>
2015-03-26 13:43:23 +0000

    Mobius transform stores results as bits in 64 bit ints instead of elements in an array

--- mobius_cube_attack.c
+++ mobius_cube_attack.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <dxgi.h>
 
 #include "cipher_helpers.h"
 #include "mobius_cube_attack.h"
@@ -17,15 +18,13 @@ Max_terms_list *mobius_find_max_terms(int max_term_limit, size_t dimension_limit
         dimensions[i] = i;
     }
     while (max_terms_list->max_term_count < max_term_limit && dimensions[dimension_limit - 1] < cipher_info->iv_size) {
-        int *zeroed_key_poly = mobius_transform(dimensions, dimension_limit, MIN_NUM_AXES, zeroed_key, cipher_info);
-        int *linear_results = mobius_is_super_poly_linear(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
+        uint64_t *zeroed_key_poly = mobius_transform(dimensions, dimension_limit, MIN_NUM_AXES, zeroed_key, cipher_info);
+        uint64_t *linear_results = mobius_is_super_poly_linear(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
         Max_terms_list* cube_max_terms = mobius_construct_max_terms(zeroed_key_poly, dimensions, dimension_limit, cipher_info);
         for (int i = 0; i < cube_max_terms->max_term_count; i++) {
             printf("checking cube terms, overall max term count %d\n", max_terms_list->max_term_count);
             Max_term* current_max_term = cube_max_terms->max_terms[i];
-            int e=0;//commenting this out cause errors, whyyyyy
-            //varibale name can be anything
-            if (current_max_term->numberOfTerms > 0 && linear_results[i] == 1) {
+            if (current_max_term->numberOfTerms > 0 && get_bit(linear_results, i+1) == 1) {
                 max_terms_list->max_term_count++;
                 //todo: use size doubling buffer to save reallocing too much
                 max_terms_list->max_terms = realloc(max_terms_list->max_terms, sizeof(Max_term) * max_terms_list->max_term_count);
@@ -52,10 +51,10 @@ Max_terms_list *mobius_find_max_terms(int max_term_limit, size_t dimension_limit
 
 //0 means is not linear
 //1 means it is
-int *mobius_is_super_poly_linear(int *zeroed_key_super_poly, int *cube_axes, int cube_dimension, const Cipher_info * const cipher_info) {
-    int *results = malloc(sizeof(int) * power(2, cube_dimension));
+uint64_t *mobius_is_super_poly_linear(uint64_t *zeroed_key_super_poly, int *cube_axes, int cube_dimension, const Cipher_info * const cipher_info) {
+    uint64_t *results = malloc(sizeof(uint64_t) * ((power(2, cube_dimension)/64)+1));
     for (int i = 0; i < power(2, cube_dimension); i++) {
-        results[i] = 1;
+        set_bit(results, 1, i);
     }
     //number of tests is arbitrary. Pick a better number based (or change test method) based on research papers
     int number_of_keys = 10;
@@ -72,17 +71,17 @@ int *mobius_is_super_poly_linear(int *zeroed_key_super_poly, int *cube_axes, int
     //to save space, compute key a, compare to keys 1-n
     //require power(2, axis)*4 values to be stored but superpolys are recomputed multiple times
     for (int key_index_1 = 0; key_index_1 < number_of_keys; key_index_1++) {
-        int *key_1_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, keys[key_index_1], cipher_info);
+        uint64_t *key_1_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, keys[key_index_1], cipher_info);
         for (int key_index_2 = key_index_1 + 1; key_index_2 < number_of_keys; key_index_2++) {
-            int *key_2_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, keys[key_index_2], cipher_info);
+            uint64_t *key_2_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, keys[key_index_2], cipher_info);
             uint64_t combined_keys[2] = {keys[key_index_1][0] ^ keys[key_index_2][0], keys[key_index_1][1] ^ keys[key_index_2][1]};
-            int *pre_combined_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, combined_keys, cipher_info);
+            uint64_t *pre_combined_superpolys = mobius_transform(cube_axes, cube_dimension, MIN_NUM_AXES, combined_keys, cipher_info);
             int all_results_non_linear = 0;
             for (int i = 0; i < power(2, cube_dimension); i++) {
-                if ((pre_combined_superpolys[i] ^ zeroed_key_super_poly[i]) != (key_1_superpolys[i] ^ key_2_superpolys[i])) {
-                    if (results[i] == 1) {
-                        all_results_non_linear += results[i];
-                        results[i] = 0;
+                if ((get_bit(pre_combined_superpolys, i) ^ get_bit(zeroed_key_super_poly, i)) !=  (get_bit(key_1_superpolys, i) ^ get_bit(key_2_superpolys, i))) {
+                    if (get_bit(results, i) == 1) {
+                        all_results_non_linear += 1;
+                        set_bit(results, 0, i);
                     }
                 }
             }
@@ -102,28 +101,29 @@ int *mobius_is_super_poly_linear(int *zeroed_key_super_poly, int *cube_axes, int
     return results;
 }
 
-Max_terms_list* mobius_construct_max_terms(int *zeroed_super_polys, int *dimensions, int dimension_count, const Cipher_info * const cipher_info) {
+Max_terms_list* mobius_construct_max_terms(uint64_t *zeroed_super_polys, int *dimensions, int dimension_count, const Cipher_info * const cipher_info) {
     uint64_t term_key[2] = {0, 0};
     Max_terms_list *max_terms_list = malloc(sizeof(Max_terms_list));
-    max_terms_list->max_term_count = power(2, dimension_count);
+    max_terms_list->max_term_count = power(2, dimension_count)-1;
     max_terms_list->max_terms = malloc( sizeof(Max_term)*max_terms_list->max_term_count);
-    for (int g = 0; g < power(2, dimension_count); g++) {
+    //start at one to avoid repeatedly calculating poly for no iv axes
+    for (int g = 1; g < power(2, dimension_count); g++) {
         Max_term *max_term = malloc(sizeof(Max_term));
         max_term->iv = iv_from_mask(g, dimensions, dimension_count, cipher_info->iv_size);
         max_term->terms = malloc(sizeof(int) * cipher_info->key_size);
         max_term->numberOfTerms = 0;
-        max_term->plusOne = zeroed_super_polys[g];
-        max_terms_list->max_terms[g] = max_term;
+        max_term->plusOne = get_bit(zeroed_super_polys, g);
+        max_terms_list->max_terms[g-1] = max_term;
     }
     for (int key_bit = 0; key_bit < cipher_info->key_size; key_bit++) {
         //increase the key bit that's set to 1
         term_key[0] = 0;
         term_key[1] = 0;
         set_bit(term_key, 1, key_bit);
-        int *key_super_poly = mobius_transform(dimensions, dimension_count, MIN_NUM_AXES, term_key, cipher_info);
-        for (int g = 0; g < power(2, dimension_count); g++) {
-            Max_term *current_max_term = max_terms_list->max_terms[g];
-            if (key_super_poly[g] + zeroed_super_polys[g] == 1) {
+        uint64_t *key_super_poly = mobius_transform(dimensions, dimension_count, MIN_NUM_AXES, term_key, cipher_info);
+        for (int g = 1; g < power(2, dimension_count); g++) {
+            Max_term *current_max_term = max_terms_list->max_terms[g-1];
+            if (get_bit(key_super_poly, g) + get_bit(zeroed_super_polys, g) == 1) {
                 current_max_term->terms[current_max_term->numberOfTerms] = key_bit;
                 current_max_term->numberOfTerms++;
             }
@@ -169,9 +169,11 @@ Max_terms_list* mobius_construct_max_terms(int *zeroed_super_polys, int *dimensi
 
 //min_num_axis is used to saved storage space because low number of axis elements are
 //very unlikly to find a maxterm
-int *mobius_transform(const int const *cube_axis, const int num_of_axis, const int min_num_axes, uint64_t *key, const Cipher_info * const cipher_info) {
+
+uint64_t *mobius_transform(const int const *cube_axis, const int num_of_axis, const int min_num_axes, uint64_t *key, const Cipher_info *const cipher_info) {
     uint64_t derivative_count = power(2, num_of_axis);
-    int *derivative_results = malloc(sizeof(int) * derivative_count);
+    uint64_t* derivative_results = malloc(sizeof(uint64_t) * ((derivative_count/64)+1));
+    //int *derivative_results = malloc(sizeof(int) * derivative_count);
     //get_super_poly_bit(key, cube_axis, cube_dimension);
     //double check this is correct for algorithm
     for (uint64_t axis_mask = 0; axis_mask < derivative_count; axis_mask++) {
@@ -179,23 +181,17 @@ int *mobius_transform(const int const *cube_axis, const int num_of_axis, const i
         int output[1] = {0};
         cipher_info->run_cipher(output, 1, current_iv, key, cipher_info->init_clocks);
         debug_print("output %d\n", output[0]);
-        derivative_results[axis_mask] = output[0];
+        set_bit(derivative_results, output[0], axis_mask);
+        //derivative_results[axis_mask] = output[0];
         free(current_iv);
-    }
-    debug_print("post init: \n");
-    for(int i =0;i< derivative_count;i++) {
-        debug_print("row %d: %d\n",i, derivative_results[i]);
     }
     //now transform output bits into superpoly bits
     for (int current_dimension = 0; current_dimension < num_of_axis; current_dimension++) {
         int size = (int) power(2, current_dimension);
         for (int pos = 0; pos < power(2, num_of_axis); pos += 2 * size) {
             for (int j = 0; j < size; j++) {
-                debug_print("table update: \n");
-                for(int i =0;i< derivative_count;i++) {
-                    debug_print("row %d: %d\n",i, derivative_results[i]);
-                }
-                derivative_results[pos + size + j] = derivative_results[pos + j] ^ derivative_results[pos + size + j];
+                int new_value = get_bit(derivative_results, pos + j) ^ get_bit(derivative_results, pos + size + j);
+                set_bit(derivative_results, new_value, pos + size + j);
             }
         }
     }
